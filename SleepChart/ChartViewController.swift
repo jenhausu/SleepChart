be67322e2be046bc2cde412b27eeea4ec11bdc95
蘇健豪 <jenhausu@icloud.com>
2017-01-10 10:35:19 +0800

    read sleepAnalysis Data

--- SleepChart/ChartViewController.swift
+++ SleepChart/ChartViewController.swift
@@ -7,11 +7,40 @@
 //
 
 import UIKit
+import HealthKit
 
 class ChartViewController: UIViewController {
     let healthStore = HealthKitClass.sharedInstance.healthStore
+    
+    var sleepDatas : [HKCategorySample] = []
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        readData()
+    }
+    
+    fileprivate func readData() {
+        let q = HKSampleQuery.init(sampleType: HKSampleType.categoryType(forIdentifier: .sleepAnalysis)!,
+                                   predicate: nil,
+                                   limit: HKObjectQueryNoLimit,
+                                   sortDescriptors: nil,
+                                   resultsHandler: {(query:HKSampleQuery, sample:[HKSample]?, error:Error?) -> Void in
+                                    if let results = sample {
+                                        for result in results {
+                                            if let sleepData = result as? HKCategorySample {
+                                                self.sleepDatas.append(sleepData)
+                                                
+                                                print(sleepData.startDate)
+                                                print(sleepData.endDate)
+                                            }
+                                        }
+                                    }
+                                    else {
+                                        // No results were returned, check the error
+                                    }
+        })
+        healthStore.execute(q)
     }
 }
