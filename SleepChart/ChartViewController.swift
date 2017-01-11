035ab50af6f9752111ebddd22664ee474830ac39
蘇健豪 <jenhausu@icloud.com>
2017-01-11 11:31:28 +0800

    處理資料
    把每週的睡眠時間做平均

--- SleepChart/ChartViewController.swift
+++ SleepChart/ChartViewController.swift
@@ -13,8 +13,9 @@ class ChartViewController: UIViewController {
     let healthStore = HealthKitClass.sharedInstance.healthStore
     
     var sleepDatas : [HKCategorySample] = []
+    var avgSleepTime : [Double] = [0, 0, 0, 0, 0, 0, 0, 0]
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,16 +32,34 @@ class ChartViewController: UIViewController {
                                         for result in results {
                                             if let sleepData = result as? HKCategorySample {
                                                 self.sleepDatas.append(sleepData)
-                                                
-                                                print(sleepData.startDate)
-                                                print(sleepData.endDate)
                                             }
                                         }
+                                        
+                                        self.processData()
                                     }
                                     else {
                                         // No results were returned, check the error
                                     }
         })
         healthStore.execute(q)
+    }
+    
+    fileprivate func processData() {
+        for index in 0...self.sleepDatas.count - 1 {
+            let goToBedTime = self.sleepDatas[index].startDate
+            let wakeUpTime = self.sleepDatas[index].endDate
+            let sleepTime = DateInterval.init(start: goToBedTime, end: wakeUpTime).duration
+            
+            let dateFormat = DateFormatter()
+            dateFormat.dateFormat = "e"
+            let e = Int(dateFormat.string(from: wakeUpTime))!
+            
+            if avgSleepTime[e] == 0 {
+                avgSleepTime[e] = sleepTime
+            }
+            else {
+                avgSleepTime[e] = (avgSleepTime[e] + sleepTime) / 2
+            }
+        }
     }
 }
